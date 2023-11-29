27151dd86814e445e0aa7194ee8a24d95760c062
James pollard <james.pollard@sciacademy.org>
2023-11-29 17:58:37 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,8 +20,10 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+             // TODO 1: Declare our variables //
+             var circle;			
+             var circles = [];	
+ 
         // TODO 2 : Create a function that draws a circle 
         
 
